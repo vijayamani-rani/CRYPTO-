75c6ed28473bfa681cce65655e93538a957d15e0
vijayamani-rani <vijayamanijuvva@gmail.com>
2025-05-28 11:15:13 +0530

    Update 02_monoalphabetic_cipher[1].c

--- 02_monoalphabetic_cipher[1].c
+++ 02_monoalphabetic_cipher[1].c
@@ -2,39 +2,38 @@
 #include <string.h>
 #include <ctype.h>
 
-char map[26];
+int main() {
+    char plaintext[100], key[100];
+    
+    printf("Enter plaintext (no spaces): ");
+    scanf("%s", plaintext);  // Reads a single word
+    
+    printf("Enter key (no spaces): ");
+    scanf("%s", key);        // Reads a single word
 
-void generateMap(char *key) {
-    int used[26] = {0}, idx = 0;
+    // Convert key to uppercase
     for (int i = 0; key[i]; i++) {
-        char ch = toupper(key[i]);
-        if (!used[ch - 'A']) {
-            map[idx++] = ch;
-            used[ch - 'A'] = 1;
+        key[i] = toupper(key[i]);
+    }
+
+    int len = strlen(plaintext);
+    int keyLen = strlen(key);
+
+    printf("Encrypted: ");
+    for (int i = 0, j = 0; i < len; i++) {
+        char ch = plaintext[i];
+        if (isalpha(ch)) {
+            int shift = key[j % keyLen] - 'A';
+            if (isupper(ch))
+                printf("%c", 'A' + (ch - 'A' + shift) % 26);
+            else
+                printf("%c", 'a' + (ch - 'a' + shift) % 26);
+            j++;
+        } else {
+            printf("%c", ch);
         }
     }
-    for (int i = 0; i < 26; i++)
-        if (!used[i])
-            map[idx++] = 'A' + i;
-}
 
-void encrypt(char *text) {
-    for (int i = 0; text[i]; i++) {
-        if (isupper(text[i]))
-            text[i] = map[text[i] - 'A'];
-        else if (islower(text[i]))
-            text[i] = tolower(map[text[i] - 'a']);
-    }
-}
-
-int main() {
-    char key[100], text[1000];
-    printf("Enter keyword: ");
-    scanf("%s", key);
-    printf("Enter plaintext: ");
-    getchar(); fgets(text, sizeof(text), stdin);
-    generateMap(key);
-    encrypt(text);
-    printf("Encrypted text: %s", text);
+    printf("\n");
     return 0;
 }
